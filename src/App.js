5f4affb9df08c639242f6e65160af1843b5bc0e6
karimzaher <karimzaher@tamu.edu>
2023-11-24 16:01:28 -0800

    display line number and file content in single txt element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box } from '@mui/material';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="background">
+        <input 
+          onChange={handleFileSelect}
+          type="file" name="file" id="file"/>
+        <pre id="fileContent"></pre>
+        <div className="lineNumberStorage">
+          <p className="lineNumber"></p>
+        </div>
       </header>
     </div>
   );
+}
+
+const filestore = [];
+console.log(document.getElementsByClassName("lineNumber"))
+
+
+function handleFileSelect(evt) {
+  const file = evt.target.files[0];
+  const reader = new FileReader();
+  document.getElementsByClassName("lineNumber")[0].textContent = ""
+  reader.onload = function (event) {
+    const fileContent = event.target.result;
+    const fileName = file.name;
+
+    const fileData = {
+      name: fileName,
+      content: fileContent
+    }
+    filestore.push(fileData)
+    console.log(filestore)
+    const fileSplit = fileContent.split('\r\n')
+    const numLines = fileContent.split('\r\n').length
+    for (let index = 0; index < numLines; index++) {
+      document.getElementsByClassName("lineNumber")[0].textContent += index +"\t" +fileSplit[index] + "\r\n"
+      console.log(index)
+    }
+    
+    document.getElementById('fileContent').textContent = fileContent;
+    console.log(fileData)
+  };
+  console.log(evt.target.files[0])
+  reader.readAsText(evt.target.files[0])
 }
 
 export default App;
